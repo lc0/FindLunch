b00f11cf25d8b6c4cd7fa120c7c6e76997ff6846
Sergii Khomenko <sergii.khomenko@stylight.com>
2015-09-18 09:53:56 +0200

    fixed last known location bug, back to old gradle

--- mobile/src/main/java/com/brainscode/nearcommunication/PlacesActivity.java
+++ mobile/src/main/java/com/brainscode/nearcommunication/PlacesActivity.java
@@ -25,8 +25,14 @@ public class PlacesActivity extends Activity {
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, (long) 1,
                 (float) 1, mLocationListener);
 
-        double latitude = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
-        double longitude = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+        double latitude = 48.1506177;
+        double longitude = 11.5470593;
+        if (mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) != null) {
+            latitude = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude();
+            longitude = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude();
+        }
+
+
 
         String coordinates = Double.toString(latitude) +","+ Double.toString(longitude);
         new FoursquareLocations(coordinates).execute();
