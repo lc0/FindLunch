2c0d14d13d999163724fe5e0806d76635af2489f
Sergii Khomenko <sergii.khomenko@stylight.com>
2015-09-18 10:33:22 +0200

    first steps for Nsd

--- mobile/src/main/java/com/brainscode/nearcommunication/FindPeople.java
+++ mobile/src/main/java/com/brainscode/nearcommunication/FindPeople.java
@@ -1,18 +1,123 @@
 package com.brainscode.nearcommunication;
 
 import android.app.Fragment;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.app.Activity;
+import android.net.nsd.NsdServiceInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FindPeople extends Fragment {
+
+    NsdHelper mNsdHelper;
+    private TextView mStatusView;
+    private Handler mUpdateHandler;
+    public static final String TAG = "nsdLunchme";
+    LunchConnection mConnection;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         inflater.inflate(R.layout.find_people, container);
         return super.onCreateView(inflater, container, savedInstanceState);
+
+        mStatusView = (TextView) findViewById(R.id.status);
+        mUpdateHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String chatLine = msg.getData().getString("msg");
+                addChatLine(chatLine);
+            }
+        };
+
+    }
+
+
+    public void clickAdvertise(View v) {
+        // Register service
+        if(mConnection.getLocalPort() > -1) {
+            mNsdHelper.registerService(mConnection.getLocalPort());
+        } else {
+            Log.d(TAG, "ServerSocket isn't bound.");
+        }
+    }
+    public void clickDiscover(View v) {
+        mNsdHelper.discoverServices();
+    }
+    public void clickConnect(View v) {
+        NsdServiceInfo service = mNsdHelper.getChosenServiceInfo();
+        if (service != null) {
+            Log.d(TAG, "Connecting.");
+            mConnection.connectToServer(service.getHost(),
+                    service.getPort());
+        } else {
+            Log.d(TAG, "No service to connect to!");
+        }
+    }
+    public void clickSend(View v) {
+        EditText messageView = (EditText) this.findViewById(R.id.chatInput);
+        if (messageView != null) {
+            String messageString = messageView.getText().toString();
+            if (!messageString.isEmpty()) {
+                mConnection.sendMessage(messageString);
+            }
+            messageView.setText("");
+        }
+    }
+    public void addChatLine(String line) {
+        mStatusView.append("\n" + line);
+    }
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "Starting.");
+        mConnection = new ChatConnection(mUpdateHandler);
+        mNsdHelper = new NsdHelper(this);
+        mNsdHelper.initializeNsd();
+        super.onStart();
+    }
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "Pausing.");
+        if (mNsdHelper != null) {
+            mNsdHelper.stopDiscovery();
+        }
+        super.onPause();
+    }
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "Resuming.");
+        super.onResume();
+        if (mNsdHelper != null) {
+            mNsdHelper.discoverServices();
+        }
+    }
+    // For KitKat and earlier releases, it is necessary to remove the
+    // service registration when the application is stopped.  There's
+    // no guarantee that the onDestroy() method will be called (we're
+    // killable after onStop() returns) and the NSD service won't remove
+    // the registration for us if we're killed.
+    // In L and later, NsdService will automatically unregister us when
+    // our connection goes away when we're killed, so this step is
+    // optional (but recommended).
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Being stopped.");
+        mNsdHelper.tearDown();
+        mConnection.tearDown();
+        mNsdHelper = null;
+        mConnection = null;
+        super.onStop();
     }
 }
