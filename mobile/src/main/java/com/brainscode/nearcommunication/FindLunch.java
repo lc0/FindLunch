c304c2c4835ae1da0746917c5667cf623e5e3207
Sergii Khomenko <sergii.khomenko@stylight.com>
2015-09-21 21:25:05 +0200

    show ip not, service name

--- mobile/src/main/java/com/brainscode/nearcommunication/FindLunch.java
+++ mobile/src/main/java/com/brainscode/nearcommunication/FindLunch.java
@@ -11,14 +11,7 @@ import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.google.android.gms.common.SignInButton;
 
-/**
- * A login screen that offers login via email/password and via Google+ sign in.
- * <p/>
- * ************ IMPORTANT SETUP NOTES: ************
- * In order for Google+ sign in to work with your app, you must first go to:
- * https://developers.google.com/+/mobile/android/getting-started#step_1_enable_the_google_api
- * and follow the steps in "Step 1" to create an OAuth 2.0 client for your package.
- */
+
 public class FindLunch extends FragmentActivity {
 
     /**
