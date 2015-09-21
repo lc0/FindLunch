0da158c060a6468a9b367bfa755d61310685ae13
Sergii Khomenko <sergii.khomenko@stylight.com>
2015-09-21 14:25:12 +0200

    fb login is back

--- mobile/src/main/java/com/brainscode/nearcommunication/LoginFragment.java
+++ mobile/src/main/java/com/brainscode/nearcommunication/LoginFragment.java
@@ -31,42 +31,37 @@ public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Intent myIntent = new Intent(getActivity(), PlacesActivity.class);
-        getActivity().startActivity(myIntent);
-
         View view = inflater.inflate(R.layout.activity_find_lunch, container, false);
-        return view;
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
 
-//        loginButton = (LoginButton) view.findViewById(R.id.login_button);
-//        loginButton.setReadPermissions("user_friends");
-//        // If using in a fragment
-//        loginButton.setFragment(this);
-//        // Other app specific specialization
-//
-//        // Callback registration
-//        callbackManager = CallbackManager.Factory.create();
-//        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-//            @Override
-//            public void onSuccess(LoginResult loginResult) {
-//                // App code
-//                // App code
-//                Log.i("WOW", "CONNECTED!!!");
-//                Intent myIntent = new Intent(getActivity(), PlacesActivity.class);
-//                getActivity().startActivity(myIntent);
-//            }
-//
-//            @Override
-//            public void onCancel() {
-//                Log.i("WOW", "onCancel!!!");
-//                // App code
-//            }
-//
-//            @Override
-//            public void onError(FacebookException exception) {
-//                Log.i("WOW", "onError!!!");
-//                // App code
-//            }
-//        });
-//        return view;
+        // Callback registration
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                // App code
+                Log.i("WOW", "CONNECTED!!!");
+                Intent myIntent = new Intent(getActivity(), PlacesActivity.class);
+                getActivity().startActivity(myIntent);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("WOW", "onCancel!!!");
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.i("WOW", "onError!!!");
+                // App code
+            }
+        });
+        return view;
     }
 }
